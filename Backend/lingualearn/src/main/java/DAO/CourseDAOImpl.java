166b11c475959a4fe1c7409a2145719fc236a057
morelliadamo <1morelliadam@gmail.com>
2025-10-20 13:05:01 +0200

    Finished working on the homepage's header and hero div. Also added CourseDAO and CourseDAOImpl on the backend.

---
+++ Backend/lingualearn/src/main/java/DAO/CourseDAOImpl.java
@@ -0,0 +1,39 @@
+package DAO;
+
+import java.util.List;
+
+import model.CourseModel;
+
+public class CourseDAOImpl implements CourseDAO {
+
+    @Override
+    public void createCourse(CourseModel course) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCourse'");
+    }
+
+    @Override
+    public CourseModel getCourseById(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCourseById'");
+    }
+
+    @Override
+    public List<CourseModel> getAllCourses() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllCourses'");
+    }
+
+    @Override
+    public void updateCourse(CourseModel course) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateCourse'");
+    }
+
+    @Override
+    public void deleteCourse(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteCourse'");
+    }
+
+}
